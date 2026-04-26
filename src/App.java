92ecffa7d0c148571e96013fd18d124d6bff93d5
Rajat Kumar <rajat28012006@gmail.com>
2026-04-26 11:53:55 +0530

    Nested classes in java

--- src/App.java
+++ src/App.java
@@ -1,11 +1,17 @@
-class Teacher{
+class Teacher {
 
-    void display(){
+    void display() {
         System.out.println("hey I am a teacher ");
     }
-    public class Student{
-        void display(){
+
+    public class Student {
+        void display() {
             System.out.println("hello student");
+        }
+        class Ram {
+            void display() {
+                System.out.println("Hey I am Ram ");
+            }
         }
     }
 };
@@ -13,8 +19,10 @@ class Teacher{
 public class App {
     public static void main(String[] args) {
         Teacher teacher = new Teacher();
-        Teacher.Student student = teacher.new Student(); 
+        Teacher.Student student = teacher.new Student();
         teacher.display();
         student.display();
+        Teacher.Student.Ram ram = student.new Ram();
+        ram.display();
     }
 }
